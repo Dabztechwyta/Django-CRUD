95edd291804f063cf3669d3779483ca178cf612b
Dabztechwrta <healervic17@gmail.com>
2022-07-04 14:49:35 +0100

    changes

--- I4G017282JUH/blog/views.py
+++ I4G017282JUH/blog/views.py
@@ -2,7 +2,7 @@ from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView, DeleteView 
 from .models import Post
 
 # Create your views here.
@@ -22,7 +22,8 @@ class PostUpdateView(UpdateView):
     fields = "__all__"
     success_url = reverse_lazy("blog:all")
 
-class PostDeleteView(UpdateView):
+class PostDeleteView(DeleteView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:all")
+
